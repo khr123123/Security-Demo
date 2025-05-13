f61cb10a23d8670530f9722c2b5abfd58f5cdbf7
3031208 <21@qwe>
2025-05-13 15:44:21 +0900

    end123

--- src/main/java/org/khr/securitydemo/controller/TestController.java
+++ src/main/java/org/khr/securitydemo/controller/TestController.java
@@ -1,8 +1,15 @@
 package org.khr.securitydemo.controller;
 
+import org.khr.securitydemo.common.ErrorCode;
+import org.khr.securitydemo.exception.BusinessException;
+import org.khr.securitydemo.manager.CounterManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author KK
@@ -11,8 +18,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @PostMapping("/order/query")
-    public String queryOrder() {
+    // 仅是为了方便，才把这段代码写到这里
+    @Resource
+    private CounterManager counterManager;
+
+    /**
+     * 检测爬虫
+     *
+     * @param loginUserId
+     */
+    private void crawlerDetect(long loginUserId) {
+        // 调用多少次时告警
+        final int WARN_COUNT = 10;
+        // 调用多少次时封号
+        final int BAN_COUNT = 20;
+        // 拼接访问 key
+        String key = String.format("user:access:%s", loginUserId);
+        // 统计一分钟内访问次数，180 秒过期
+        long count = counterManager.incrAndGetCounter(key, 1, TimeUnit.MINUTES, 180);
+        // 是否封号
+        if (count > BAN_COUNT) {
+            // 封号
+            // User updateUser = new User();
+            // updateUser.setId(loginUserId);
+            // updateUser.setUserRole("ban");
+            // userService.updateById(updateUser);
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR, "访问次数过多，已被封号");
+        }
+        // 是否告警
+        if (count == WARN_COUNT) {
+            // 可以改为向管理员发送邮件通知
+            throw new BusinessException(110, "警告：访问太频繁");
+        }
+    }
+
+    @PostMapping("/order/query/{id}")
+    public String queryOrder(@PathVariable long id) {
+        crawlerDetect(id);
         return "/order/query";
     }
 
